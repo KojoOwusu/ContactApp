127f390225b6a023e7274b618f1d8ae14e446d49
Kojo Owusu <owusukojo97@gmail.com>
2020-09-14 11:19:45 +0000

    created Contact database models and made migrations and generated schema

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,14 @@
 class Contact < ApplicationRecord
     validates :firstname, presence: true
     validates :lastname, presence:true
+    has_many :phonenumbers, dependent: :destroy
+    has_many :emails, dependent: :destroy
+end
+
+class Email < ApplicationRecord
+    belongs_to :contact
+end
+
+class Phonenumber < ApplicationRecord
+    belongs_to :contact
 end
--- db/migrate/20200914005948_create_contacts.rb
+++ db/migrate/20200914005948_create_contacts.rb
@@ -3,10 +3,21 @@ class CreateContacts < ActiveRecord::Migration[6.0]
     create_table :contacts do |t|
       t.string :firstname, null: false
       t.string :lastname, null: false
-      t.string :phonenumbers, array: true, default: []
-      t.string :emails, array:true, default: []
       t.string :twitterusername
-      
+      t.timestamps
+    end
+
+    create_table :phonenumbers do |t|
+      t.belongs_to :contact
+      t.string :phonenumber, null: false
+      t.string :purpose
+      t.timestamps
+    end
+
+    create_table :emails do |t|
+      t.belongs_to :contact
+      t.string :email
+      t.string :purpose 
       t.timestamps
     end
   end
