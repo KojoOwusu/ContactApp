b01ead975546490f76f28a7919de23e341cfb3a5
Kwadwo owusu <owusukojo97@gmail.com>
2020-09-18 22:52:07 +0000

    created react build app and config scripts

---
+++ contactapp-frontend/build/precache-manifest.bc5f614568939a140ac94e16863cedd8.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "251e7120a5b2d3364404ce6c445a5a44",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3e84c21f0035043e8a54",
+    "url": "/static/css/2.99a5358a.chunk.css"
+  },
+  {
+    "revision": "a77bd7c1dca6062d8297",
+    "url": "/static/css/main.e23f745a.chunk.css"
+  },
+  {
+    "revision": "3e84c21f0035043e8a54",
+    "url": "/static/js/2.55bef74b.chunk.js"
+  },
+  {
+    "revision": "b5901214077ea32a7534a1d4eff7c84a",
+    "url": "/static/js/2.55bef74b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a77bd7c1dca6062d8297",
+    "url": "/static/js/main.8bef27ef.chunk.js"
+  },
+  {
+    "revision": "12575906988c62e2ea1d",
+    "url": "/static/js/runtime-main.22012b4c.js"
+  },
+  {
+    "revision": "4ac4470c53aa1af53e1f624a153c76bb",
+    "url": "/static/media/email.4ac4470c.svg"
+  },
+  {
+    "revision": "0cd1fbd3be45c5d270f5b893bee87fb8",
+    "url": "/static/media/phone.0cd1fbd3.svg"
+  },
+  {
+    "revision": "34ef862a66f1df42775f534157d5d910",
+    "url": "/static/media/twitter.34ef862a.svg"
+  },
+  {
+    "revision": "68f2a6aace3cfd74492767e21772cffe",
+    "url": "/static/media/user.68f2a6aa.svg"
+  }
+]);
--- contactapp-frontend/src/shared/constants/constants.ts
+++ contactapp-frontend/src/shared/constants/constants.ts
@@ -1,3 +1,4 @@
 export const Constants = {
 	GRAPHQL_API_URI: "http://localhost:3090/graphql",
+	HEROKU_API_ENDPOINT: "https://safe-earth-99320.herokuapp.com",
 };
