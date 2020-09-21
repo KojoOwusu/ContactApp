0e1b76e1ad5bc14c25a0ef7221ba60ad71ff2a38
Kwadwo owusu <owusukojo97@gmail.com>
2020-09-21 14:59:10 +0000

    fixed floatin gaction button positionining

--- contactapp-frontend/src/components/contactListComponent/contactListComponent.tsx
+++ contactapp-frontend/src/components/contactListComponent/contactListComponent.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState, Fragment } from "react";
 import "./styles.css";
-import PlusSvg from "../../assets/svgs/plus.svg";
 import { Button, Divider } from "antd";
 import { BrowserRouter as Router, Switch, Route, Link, useHistory } from "react-router-dom";
 import { PlusOutlined } from "@ant-design/icons";
@@ -20,17 +19,9 @@ const renderContact = (serverData: { id: number; firstname: string }) => {
 
 const AddButton: React.FC = () => {
 	return (
-		<div>
-			<Link to="/addContact">
-				<Button
-					className="AddButton"
-					type="primary"
-					shape="circle"
-					icon={<PlusOutlined />}
-					size="large"
-				/>
-			</Link>
-		</div>
+		<Link to="/addContact" className="AddButton">
+			<Button type="primary" shape="circle" icon={<PlusOutlined />} size="large" />
+		</Link>
 	);
 };
 
--- contactapp-frontend/src/config/apolloClientsetup.ts
+++ contactapp-frontend/src/config/apolloClientsetup.ts
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
 import { Constants as C } from "../shared/constants/constants";
 
 //Apollo cient for handling all requests to graphql api
-const link = createHttpLink({ uri: "https://safe-earth-99320.herokuapp.com/graphql" });
+const link = createHttpLink({ uri: "http://localhost:3090/graphql" });
 
 export const client = new ApolloClient({
 	link: link,
